1c29c0bd09a6bcd60f38d7b867d9c700a07b84d9
Vihaan Shah <vihaan40@gmail.com>
2024-03-20 15:30:03 -0400

    Rename arm_0 and arm_1 to left and right respectively

--- dual_ur_robot_driver/launch/dual_ur_control.launch.py
+++ dual_ur_robot_driver/launch/dual_ur_control.launch.py
@@ -12,26 +12,26 @@ from launch_ros.actions import PushRosNamespace
 
 def generate_launch_description():
 
-    ur_type = LaunchConfiguration('ur_type') 
-    arm_0_robot_ip = LaunchConfiguration('arm_0_robot_ip') 
-    arm_0_controller_file = LaunchConfiguration('arm_0_controller_file') 
-    arm_0_tf_prefix = LaunchConfiguration('arm_0_tf_prefix')
-    arm_0_use_fake_hardware = LaunchConfiguration('arm_0_use_fake_hardware')
-    arm_0_launch_rviz = LaunchConfiguration('arm_0_launch_rviz')
-    arm_0_script_command_port = LaunchConfiguration('arm_0_script_command_port')
-    arm_0_trajectory_port = LaunchConfiguration('arm_0_trajectory_port')
-    arm_0_reverse_port = LaunchConfiguration('arm_0_reverse_port')
-    arm_0_script_sender_port = LaunchConfiguration('arm_0_script_sender_port')
+    ur_type = LaunchConfiguration('ur_type')
+    left_robot_ip = LaunchConfiguration('left_robot_ip')
+    left_controller_file = LaunchConfiguration('left_controller_file')
+    left_tf_prefix = LaunchConfiguration('left_tf_prefix')
+    left_use_fake_hardware = LaunchConfiguration('left_use_fake_hardware')
+    left_launch_rviz = LaunchConfiguration('left_launch_rviz')
+    left_script_command_port = LaunchConfiguration('left_script_command_port')
+    left_trajectory_port = LaunchConfiguration('left_trajectory_port')
+    left_reverse_port = LaunchConfiguration('left_reverse_port')
+    left_script_sender_port = LaunchConfiguration('left_script_sender_port')
 
-    arm_1_robot_ip = LaunchConfiguration('arm_1_robot_ip') 
-    arm_1_controller_file = LaunchConfiguration('arm_1_controller_file') 
-    arm_1_tf_prefix = LaunchConfiguration('arm_1_tf_prefix')
-    arm_1_use_fake_hardware = LaunchConfiguration('arm_1_use_fake_hardware')
-    arm_1_launch_rviz = LaunchConfiguration('arm_1_launch_rviz')
-    arm_1_script_command_port = LaunchConfiguration('arm_1_script_command_port')
-    arm_1_trajectory_port = LaunchConfiguration('arm_1_trajectory_port')
-    arm_1_reverse_port = LaunchConfiguration('arm_1_reverse_port')
-    arm_1_script_sender_port = LaunchConfiguration('arm_1_script_sender_port')
+    right_robot_ip = LaunchConfiguration('right_robot_ip') 
+    right_controller_file = LaunchConfiguration('right_controller_file')
+    right_tf_prefix = LaunchConfiguration('right_tf_prefix')
+    right_use_fake_hardware = LaunchConfiguration('right_use_fake_hardware')
+    right_launch_rviz = LaunchConfiguration('right_launch_rviz')
+    right_script_command_port = LaunchConfiguration('right_script_command_port')
+    right_trajectory_port = LaunchConfiguration('right_trajectory_port')
+    right_reverse_port = LaunchConfiguration('right_reverse_port')
+    right_script_sender_port = LaunchConfiguration('right_script_sender_port')
 
     # # UR specific arguments
     ur_type_arg = DeclareLaunchArgument(
@@ -40,96 +40,96 @@ def generate_launch_description():
             description="Type/series of used UR robot.",
             choices=["ur3", "ur3e", "ur5", "ur5e", "ur10", "ur10e", "ur16e", "ur20"],
     )
-    arm_0_robot_ip_arg = DeclareLaunchArgument(
-            "arm_0_robot_ip",
+    left_robot_ip_arg = DeclareLaunchArgument(
+            "left_robot_ip",
             default_value='xxx.xxx.xxx.xxx',
             description="IP address by which the robot can be reached.",
     )
-    arm_0_controller_file_arg = DeclareLaunchArgument(
-            "arm_0_controller_file",
-            default_value="arm_0_ur_controllers.yaml",
+    left_controller_file_arg = DeclareLaunchArgument(
+            "left_controller_file",
+            default_value="left_ur_controllers.yaml",
             description="YAML file with the controllers configuration.",
     )
-    arm_0_tf_prefix_arg = DeclareLaunchArgument(
-            "arm_0_tf_prefix",
-            default_value="arm_0_",
+    left_tf_prefix_arg = DeclareLaunchArgument(
+            "left_tf_prefix",
+            default_value="left_",
             description="tf_prefix of the joint names, useful for \
             multi-robot setup. If changed, also joint names in the controllers' configuration \
             have to be updated.",
     )
-    arm_0_use_fake_hardware_arg = DeclareLaunchArgument(
-            "arm_0_use_fake_hardware",
+    left_use_fake_hardware_arg = DeclareLaunchArgument(
+            "left_use_fake_hardware",
             default_value="true",
     )
-    arm_0_launch_rviz_arg = DeclareLaunchArgument(
-            "arm_0_launch_rviz",
+    left_launch_rviz_arg = DeclareLaunchArgument(
+            "left_launch_rviz",
             default_value="false",
     )
-    arm_0_script_command_port_arg =  DeclareLaunchArgument(
-            "arm_0_script_command_port",
+    left_script_command_port_arg =  DeclareLaunchArgument(
+            "left_script_command_port",
             default_value="50002",
             description="Port that will be opened to forward script commands from the driver to the robot",
         )
-    arm_0_trajectory_port_arg = DeclareLaunchArgument(
-            "arm_0_trajectory_port",
+    left_trajectory_port_arg = DeclareLaunchArgument(
+            "left_trajectory_port",
             default_value="50003",
             description="Port that will be opened to forward script commands from the driver to the robot",
         )
-    arm_0_reverse_port_arg = DeclareLaunchArgument(
-            "arm_0_reverse_port",
+    left_reverse_port_arg = DeclareLaunchArgument(
+            "left_reverse_port",
             default_value="50001",
             description="Port that will be opened to forward script commands from the driver to the robot",
         )
-    arm_0_script_sender_port_arg = DeclareLaunchArgument(
-            "arm_0_script_sender_port",
+    left_script_sender_port_arg = DeclareLaunchArgument(
+            "left_script_sender_port",
             default_value="50005",
             description="Port that will be opened to forward script commands from the driver to the robot",
         )
 
 
-    arm_1_robot_ip_arg = DeclareLaunchArgument(
-            "arm_1_robot_ip",
+    right_robot_ip_arg = DeclareLaunchArgument(
+            "right_robot_ip",
             default_value='yyy.yyy.yyy.yyy',
             description="IP address by which the robot can be reached.",
     )
-    arm_1_controller_file_arg = DeclareLaunchArgument(
-            "arm_1_controller_file",
-            default_value="arm_1_ur_controllers.yaml",
+    right_controller_file_arg = DeclareLaunchArgument(
+            "right_controller_file",
+            default_value="right_ur_controllers.yaml",
             description="YAML file with the controllers configuration.",
     )
-    arm_1_tf_prefix_arg = DeclareLaunchArgument(
-            "arm_1_tf_prefix",
-            default_value="arm_1_",
+    right_tf_prefix_arg = DeclareLaunchArgument(
+            "right_tf_prefix",
+            default_value="right_",
             description="tf_prefix of the joint names, useful for \
             multi-robot setup. If changed, also joint names in the controllers' configuration \
             have to be updated.",
     )
-    arm_1_use_fake_hardware_arg = DeclareLaunchArgument(
-            "arm_1_use_fake_hardware",
+    right_use_fake_hardware_arg = DeclareLaunchArgument(
+            "right_use_fake_hardware",
             default_value="true",
     )
-    arm_1_launch_rviz_arg = DeclareLaunchArgument(
-            "arm_1_launch_rviz",
+    right_launch_rviz_arg = DeclareLaunchArgument(
+            "right_launch_rviz",
             default_value="false",
     )
-    arm_1_script_command_port_arg =  DeclareLaunchArgument(
-            "arm_1_script_command_port",
+    right_script_command_port_arg =  DeclareLaunchArgument(
+            "right_script_command_port",
             default_value="50010",
             description="Port that will be opened to forward script commands from the driver to the robot",
     )
 
-    arm_1_trajectory_port_arg = DeclareLaunchArgument(
-            "arm_1_trajectory_port",
+    right_trajectory_port_arg = DeclareLaunchArgument(
+            "right_trajectory_port",
             default_value="50009",
             description="Port that will be opened to forward script commands from the driver to the robot",
         )
-    arm_1_reverse_port_arg = DeclareLaunchArgument(
-            "arm_1_reverse_port",
+    right_reverse_port_arg = DeclareLaunchArgument(
+            "right_reverse_port",
             default_value="50006",
             description="Port that will be opened to forward script commands from the driver to the robot",
         )
-    arm_1_script_sender_port_arg = DeclareLaunchArgument(
-            "arm_1_script_sender_port",
+    right_script_sender_port_arg = DeclareLaunchArgument(
+            "right_script_sender_port",
             default_value="50007",
             description="Port that will be opened to forward script commands from the driver to the robot",
         )
@@ -137,69 +137,69 @@ def generate_launch_description():
 
     ur_launch_dir = get_package_share_directory('dual_ur_robot_driver')
 
-    arm_0 = IncludeLaunchDescription(
+    left = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(os.path.join(ur_launch_dir, 'launch', 'ur_control.launch.py')),
         launch_arguments={'ur_type': ur_type,
-                          'robot_ip': arm_0_robot_ip,
-                          'controllers_file': arm_0_controller_file,
-                          'tf_prefix': arm_0_tf_prefix,
-                          'use_fake_hardware': arm_0_use_fake_hardware,
-                          'launch_rviz': arm_0_launch_rviz,
-                          'script_command_port': arm_0_script_command_port,
-                          'trajectory_port': arm_0_trajectory_port,
-                          'reverse_port': arm_0_reverse_port,
-                          'script_sender_port': arm_0_script_sender_port,
+                          'robot_ip': left_robot_ip,
+                          'controllers_file': left_controller_file,
+                          'tf_prefix': left_tf_prefix,
+                          'use_fake_hardware': left_use_fake_hardware,
+                          'launch_rviz': left_launch_rviz,
+                          'script_command_port': left_script_command_port,
+                          'trajectory_port': left_trajectory_port,
+                          'reverse_port': left_reverse_port,
+                          'script_sender_port': left_script_sender_port,
                           }.items())
     
-    arm_0_with_namespace = GroupAction(
+    left_with_namespace = GroupAction(
      actions=[
-         PushRosNamespace('arm_0'),
-         arm_0
+         PushRosNamespace('left'),
+         left
       ]
      
     )
 
-    arm_1 = IncludeLaunchDescription(
+    right = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(os.path.join(ur_launch_dir, 'launch', 'ur_control.launch.py')),
         launch_arguments={'ur_type': ur_type,
-                          'robot_ip': arm_1_robot_ip,
-                          'controllers_file': arm_1_controller_file,
-                          'tf_prefix': arm_1_tf_prefix,
-                          'use_fake_hardware': arm_1_use_fake_hardware,
-                          'launch_rviz': arm_1_launch_rviz,
-                          'script_command_port': arm_1_script_command_port,
-                          'trajectory_port': arm_1_trajectory_port,
-                          'reverse_port': arm_1_reverse_port,
-                          'script_sender_port': arm_1_script_sender_port,
+                          'robot_ip': right_robot_ip,
+                          'controllers_file': right_controller_file,
+                          'tf_prefix': right_tf_prefix,
+                          'use_fake_hardware': right_use_fake_hardware,
+                          'launch_rviz': right_launch_rviz,
+                          'script_command_port': right_script_command_port,
+                          'trajectory_port': right_trajectory_port,
+                          'reverse_port': right_reverse_port,
+                          'script_sender_port': right_script_sender_port,
                           }.items())
     
-    arm_1_with_namespace = GroupAction(
+    right_with_namespace = GroupAction(
      actions=[
-         PushRosNamespace('arm_1'),
-         arm_1
+         PushRosNamespace('right'),
+         right
       ]
     )
     
     return LaunchDescription([
         ur_type_arg,
-        arm_0_robot_ip_arg,
-        arm_0_controller_file_arg,
-        arm_0_tf_prefix_arg,
-        arm_0_use_fake_hardware_arg,
-        arm_0_launch_rviz_arg,
-        arm_0_script_command_port_arg,
-        arm_0_trajectory_port_arg,
-        arm_0_reverse_port_arg,
-        arm_0_script_sender_port_arg,
-        arm_1_robot_ip_arg,
-        arm_1_controller_file_arg,
-        arm_1_tf_prefix_arg,
-        arm_1_use_fake_hardware_arg,
-        arm_1_launch_rviz_arg,
-        arm_1_script_command_port_arg,
-        arm_1_trajectory_port_arg,
-        arm_1_reverse_port_arg,
-        arm_1_script_sender_port_arg,
-        arm_0_with_namespace,
-        arm_1_with_namespace
+        left_robot_ip_arg,
+        left_controller_file_arg,
+        left_tf_prefix_arg,
+        left_use_fake_hardware_arg,
+        left_launch_rviz_arg,
+        left_script_command_port_arg,
+        left_trajectory_port_arg,
+        left_reverse_port_arg,
+        left_script_sender_port_arg,
+        right_robot_ip_arg,
+        right_controller_file_arg,
+        right_tf_prefix_arg,
+        right_use_fake_hardware_arg,
+        right_launch_rviz_arg,
+        right_script_command_port_arg,
+        right_trajectory_port_arg,
+        right_reverse_port_arg,
+        right_script_sender_port_arg,
+        left_with_namespace,
+        right_with_namespace
     ])
